b8ef4392846474b049d6c230021c95a7bacd9291
Harpo <92917966+helveticka@users.noreply.github.com>
2024-05-30 17:04:01 +0200

    Comentaris PlayedWords

--- app/src/main/java/com/example/zenword/HiddenWords.java
+++ app/src/main/java/com/example/zenword/HiddenWords.java
@@ -20,12 +20,16 @@ public class HiddenWords
     private final int[] guidelines;
     private TextView[][] wordsTextViews;
 
-
+    /**
+     * Constructor de la classe HiddenWords
+     * @param mainActivity activitat principal de l'aplicació
+     */
     public HiddenWords(MainActivity mainActivity)
     {
         this.mainActivity = mainActivity;
         guidelines = new int[6];
 
+        //Comprovam si la disposició de la pantalla és vertical o horitzontal
         if (MainActivity.isVertical)
         {
             guidelines[0] = mainActivity.findViewById(R.id.guidelineHor1).getId();
@@ -78,12 +82,18 @@ public class HiddenWords
         }
     }
 
-
+    /**
+     * Crea una fila de TextViews que servirà per mostrar una paraula
+     * @param guia número de guideline on crear la fila
+     * @param lletres número de lletres que s'han de mostrar
+     * @return array de TextViews correctament col.locat
+     */
     private TextView[] crearFilaTextViews(int guia, int lletres)
     {
         TextView[] param = new TextView[lletres];
         ConstraintLayout constraint = mainActivity.findViewById(R.id.main);
 
+        //Definim les propietats de cada TextView
         for (int i=0; i<lletres; i++)
         {
             int id = View.generateViewId();
@@ -101,6 +111,7 @@ public class HiddenWords
             ConstraintSet constraintSet = new ConstraintSet();
             constraintSet.clone(constraint);
 
+            //Actualitzam les dimensions en funció de la disposició de la pantalla
             int wdth, startId, maxHeight;
             if (MainActivity.isVertical)
             {
@@ -119,13 +130,13 @@ public class HiddenWords
 
             int margin = (int) ((wdth*(7-lletres)+28*MainActivity.density)/2);
 
-
             constraintSet.connect(id, ConstraintSet.BOTTOM, guidelines[guia+1], ConstraintSet.TOP,4);
             constraintSet.connect(id, ConstraintSet.TOP, guidelines[guia], ConstraintSet.BOTTOM,4);
 
             constraintSet.constrainHeight(id, maxHeight);
             constraintSet.constrainWidth(id, wdth);
 
+            //Definim el constraint dels TextViews
             if (i == 0)
             {
                 constraintSet.connect(id, ConstraintSet.START, startId, ConstraintSet.START, margin);
@@ -141,7 +152,11 @@ public class HiddenWords
         return param;
     }
 
-
+    /**
+     * Mostra una paraula sencera
+     * @param s paraula a mostrar
+     * @param posicio posició de la paraula
+     */
     public void mostraParaula(String s, int posicio)
     {
         for (int i=0; i<wordsTextViews[posicio].length; i++)
@@ -151,14 +166,20 @@ public class HiddenWords
         }
     }
 
-
+    /**
+     * Mostra la primera lletra d'una paraula
+     * @param s paraula a mostrar
+     * @param posicio posició de la paraula
+     */
     public void mostraPrimeraLletra(String s, int posicio)
     {
         String c = String.valueOf(s.charAt(0)).toLowerCase();
         wordsTextViews[posicio][0].setText(c);
     }
 
-
+    /**
+     * Desactiva la visibilitat de les lletres
+     */
     public void amaga()
     {
         for (TextView[] wordsTextView : wordsTextViews)
